a2d905bf94464f5d1b7d07406c255a0f351ed7cd
Ahmad Furqan <157381667+ahmadfurqan7@users.noreply.github.com>
2024-02-21 10:19:27 +0700

    Add files via upload

---
+++ Analysis_of_the_2015_World_population/Analysis World Population - Challenge.sql
@@ -0,0 +1,14 @@
+/*In this SQL project, you will use the dataset by CIA World Factbook 
+ explore how the world population spreads across different countries.*/
+
+-- You will learn how to use SQL to answer the following analytical questions:
+
+-- Which country has the highest population?
+
+-- Which country has the least number of people?
+
+-- Which country is witnessing the highest population growth?
+
+-- Which country has an extraordinary number for the population?
+
+-- Which is the most densely populated country in the world?
